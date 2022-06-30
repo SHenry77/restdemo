1a6ece96482fa66e42078b3f9b7cda0066be122f
SHenry <SHenry77@users.noreply.github.com>
2022-06-30 14:14:56 +0100

    Initial commit

---
+++ src/test/java/com/jpmorgan/restdemo/SimpleRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jpmorgan.restdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
